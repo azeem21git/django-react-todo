423a3fbcc3f2e493b186d9f6ce4ca7b702e527cf
azeem21git <azeem21uni@gmail.com>
2026-01-09 22:04:38 +0530

    changing admin.py

--- backend/todo/apps.py
+++ backend/todo/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
+
+
+from django.apps import AppConfig
+
+
+class TodoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todo'
