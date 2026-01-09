0a21bd9080b963f5f6cfe12ec9785645543f2b88
azeem21git <azeem21uni@gmail.com>
2026-01-09 22:02:21 +0530

    ##

--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -7,3 +7,14 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'completed')
+
+
+from rest_framework import serializers
+
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+   
