c4c1929466a78ce6bedeed390aa9c4acf049003d
azeem21git <azeem21uni@gmail.com>
2026-01-13 11:37:04 +0530

    change idea

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -3,16 +3,4 @@ from django.contrib import admin
 from .models import Todo
 
 
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
 
-
-from django.contrib import admin
-
-from .models import Todo
-
-
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-   
