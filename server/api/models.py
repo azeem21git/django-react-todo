be59bbc70c11d45b8d11ecf97448f39afac00152
azeem21git <azeem21uni@gmail.com>
2026-01-13 13:44:40 +0530

    add migrate

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    tittle =models.CharField(max_length=50)
+    release_Year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.tittle
