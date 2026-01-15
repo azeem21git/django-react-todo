f72f28757b804b3ac0d8b986b496f2644852814c
azeem21git <azeem21uni@gmail.com>
2026-01-15 22:04:48 +0530

    react and server connect

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [books,setBooks] = useState([])
+  const fetchBook=async()=>{
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/books')
+      const data=await response.json()
+      setBooks(response.data)
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <div>
+    <>
       <h1>Book shop Booling</h1>
-    </div>
+
+      <div>
+        <input type="text" placeholder='Book Tittle...' />
+        <input type="number" placeholder='Release Date...' />
+        <button>Add Book</button>
+      </div>
+
+      
+        <table>
+          <thead>
+            <td>Book Tittle</td>
+            <td>Release Date</td>
+          </thead>
+
+          <tbody>
+            <tr id='books.id'>
+              {
+                 books.map((item)=>{
+                  <td>{item.tittle}</td>
+                 })
+              }
+            </tr>
+          </tbody>
+        </table>
+      
+    </>
   )
 }
 
