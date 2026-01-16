dd523685ee58f1eff79e451fab5b1fa281abaaca
azeem21git <azeem21uni@gmail.com>
2026-01-16 23:34:27 +0530

    **

--- server/backend/views.py
+++ server/backend/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Users
+from .serializer import UserSerializer
+
+@api_view
 
 
