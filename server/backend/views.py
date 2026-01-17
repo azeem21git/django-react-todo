1ee7a001d51f734cb8fba7a0cc5ddb6e04fa8784
azeem21git <azeem21uni@gmail.com>
2026-01-17 22:51:29 +0530

    not  show

--- server/backend/views.py
+++ server/backend/views.py
@@ -1,9 +1,2 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Users
-from .serializer import UserSerializer
-
-@api_view
 
 
